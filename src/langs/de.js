e95523285727e0532177e2873ce7e083a17d383d
dbarlowe <dennis@dennzo.com>
2019-11-14 22:40:49 +0100

    Added German Locale (de)

---
+++ src/langs/de.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.de = {
+  description: 'Wir verwenden Cookies, um Ihnen ein besseres Browsererlebnis zu bieten, Inhalte und Anzeigen zu personalisieren, Social Media-Funktionen bereitzustellen und unseren Traffic zu analysieren. Lesen Sie, wie wir Cookies verwenden und wie Sie diese steuern können, indem Sie auf Cookie-Einstellungen klicken. Sie stimmen unseren Cookies zu, wenn Sie diese Website weiterhin nutzen.',
+  settings: 'Cookie Einstellungen',
+  accept: 'Cookies akzeptieren',
+  statement: 'Datenschutzerklärung',
+  save: 'Speichern',
+  always_on: 'Immer aktiviert',
+  cookie_essential_title: 'Essentielle Cookies',
+  cookie_essential_desc: 'Notwendige Cookies helfen, eine Website nutzbar zu machen, indem sie Grundfunktionen wie die Seitennavigation und den Zugriff auf sichere Bereiche der Website ermöglichen. Ohne diese Cookies kann die Website nicht richtig funktionieren.',
+  cookie_performance_title: 'Performance Cookies',
+  cookie_performance_desc: 'Diese Cookies werden verwendet, um die Leistung und Funktionalität unserer Websites zu verbessern, sind aber für deren Verwendung nicht erforderlich. Zum Beispiel speichert es Ihre bevorzugte Sprache oder die Region, in der Sie sich befinden.',
+  cookie_analytics_title: 'Analytics Cookies',
+  cookie_analytics_desc: 'Wir verwenden Analyse-Cookies, um zu messen, wie Benutzer mit Website-Inhalten interagieren, was uns hilft, unsere Websites und Anwendungen für Sie anzupassen, um Ihr Erlebnis zu verbessern.',
+  cookie_marketing_title: 'Marketing Cookies',
+  cookie_marketing_desc: 'Diese Cookies werden verwendet, um Werbebotschaften für Sie und Ihre Interessen relevanter zu gestalten. Ziel ist es, Anzeigen zu schalten, die für den einzelnen Nutzer relevant und ansprechend sind und damit für Publisher und Drittwerber wertvoller sind.'
+};
